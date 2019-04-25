1c4209a77fc30e04252cff1b70e2a999aacfb3b5
deviltt <tt416926063@qq.com>
2019-04-25 23:36:25 +0800

    js反转字符串

---
+++ js/test4.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    var result = str.split("").reverse().join("");
+    return result;
+}
+console.log(reverseString("hello world"));
+// var name = "My city is WH";
+// var resultStr = name.split('').reverse().join('');
+// console.log(resultStr);  // HW si ytic yM
