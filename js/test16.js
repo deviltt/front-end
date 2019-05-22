9fa38a370705741147934a7856473f6ff5387950
deviltt <tt416926063@qq.com>
2019-05-22 11:01:08 +0800

    使用getOwnPropertyDescriptor的方法来读取数据属性和访问器属性的对象属性

--- js/test16.js
+++ js/test16.js
@@ -23,3 +23,13 @@ Object.defineProperties(book, {
 });
 book.year=2005;
 console.log(book.edition);
+
+//读取数据属性的对象属性
+var descriptor=Object.getOwnPropertyDescriptor(book, "_year");
+console.log(descriptor.configurable);
+console.log(descriptor.value);
+console.log(descriptor.enumerable);
+console.log(descriptor.writable);
+
+var descriptor1=Object.getOwnPropertyDescriptor(book, "year");
+console.log(typeof descriptor1.get);
